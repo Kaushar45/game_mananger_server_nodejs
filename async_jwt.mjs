992f999656b10336bff13056717aa2cdf9954a60
Kaushar45 <45kaushar@gmail.com>
2025-09-10 20:31:31 +0530

    asyncJwtSign function definition and export

--- async_jwt.mjs
+++ async_jwt.mjs
@@ -1,5 +1,13 @@
-const asyncjwtSign = () => {
-  return new promise();
+import jwt from "jsonwebtoken";
+const asyncJwtSign = (payload, secret) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, secret, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(token);
+    });
+  });
 };
 
-export { asyncjwtSign };
+export { asyncJwtSign };
