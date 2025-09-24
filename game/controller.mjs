941167145d052ae39cc1f7712faf140fab4b367e
Kaushar45 <45kaushar@gmail.com>
2025-09-24 19:47:00 +0530

    implement requestGame function and update game router with new endpoint

--- game/controller.mjs
+++ game/controller.mjs
@@ -1,4 +1,5 @@
-import prisma from "../prisma/db.mjs";
+import { ServerError } from "../error.mjs";
+import prisma, { DB_ERR_CODES } from "../prisma/db.mjs";
 
 const addGame = async (req, res, next) => {
   const game = await prisma.game.create({
@@ -16,4 +17,45 @@ const listGame = async (req, res, next) => {
   res.json({ msg: "successful", games });
 };
 
-export { addGame, listGame };
+const requestGame = async (req, res, name) => {
+  if (!req.body.gameID) {
+    throw new ServerError(400, "Game ID must be Supplied");
+  }
+
+  let gameSession = await prisma.gameSession.findFirst({
+    where: { gameID: req.body.gameID, status: "WAITING" },
+  });
+
+  if (!gameSession) {
+    gameSession = await prisma.gameSession.create({
+      data: {
+        gameID: req.body.gameID,
+      },
+    });
+  }
+  try {
+    const gameSessionPlayer = await prisma.gameSessionPlayer.create({
+      data: {
+        sessionID: gameSession.id,
+        playerID: req.user.id,
+      },
+    });
+
+    res.json({
+      msg: "successful",
+      gameID: req.body.gameID,
+      gameSession,
+      gameSessionPlayer,
+    });
+  } catch (err){
+    if (err.code === DB_ERR_CODES.UNIQUE_ERR) {
+      throw new ServerError(
+        400,
+        "player is already exist in this game session"
+      );
+    }
+    throw err;
+  }
+};
+
+export { addGame, listGame, requestGame };
--- prisma/db.mjs
+++ prisma/db.mjs
@@ -3,3 +3,7 @@ import { PrismaClient } from "../generated/prisma/index.js";
 const prisma = new PrismaClient();
 
 export default prisma;
+
+export const DB_ERR_CODES = {
+  UNIQUE_ERR: "P2002",
+};
